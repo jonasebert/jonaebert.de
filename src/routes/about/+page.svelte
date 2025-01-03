49c9338356f4d81573738c262c40f910c49597f5
Jona Ebert <kontakt@jonaebert.de>
2025-01-03 18:10:11 +0100

    Redesign Curriculum vitae

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,37 +1,39 @@
 <script lang="ts">
-	import Social from "$lib/components/blocks/Social.svelte";
-	import Image from "$lib/components/image.svelte";
-	import { name, pronouns } from "$lib/store";
-	import { FormatDate } from "$lib/util/date";
-	import Tags from "$lib/components/blocks/Tags.svelte";
-	export let data;
-	const { headings, items } = data.props;
+	import Social from "$lib/components/blocks/Social.svelte"
+	import Image from "$lib/components/image.svelte"
+	import { name, pronouns } from "$lib/store"
+	import { FormatDate } from "$lib/util/date"
+	import Tags from "$lib/components/blocks/Tags.svelte"
+	export let data
+	const { headings, items } = data.props
 
 
 	// Get current month + year
-	const now = new Date();
-    const nowFormatted = now.getFullYear() + "-" + (now.getMonth()+1).toString().padStart(2, '0');
+	const now = new Date()
+    const nowFormatted = now.getFullYear() + "-" + (now.getMonth()+1).toString().padStart(2, '0')
 
-	// Generate date
+	// Validate date
 	//
 	// @param {string} start
 	// @param {string} end
 	//
-	function genDate(start, end) {
-		let date = 'date not found';
+	function valDate(start: string, end: string) {
+		let date	=	'date not found'
+		let active	=	false
 
 		if (start && end) {
-			date = FormatDate(start,'month') + ' ' + FormatDate(start,'year') + ' - ' + FormatDate(end,'month') + ' ' + FormatDate(end,'year');
+			date = FormatDate(start,'month') + ' ' + FormatDate(start,'year') + ' - ' + FormatDate(end,'month') + ' ' + FormatDate(end,'year')
 		} else if (start && !end) {
 			if (start <= nowFormatted) {
-				date = 'seit ';
+				date = 'seit '
+				active = true
 			} else if (start > nowFormatted) {
-				date = 'ab ';
+				date = 'ab '
 			}
-			date = date + FormatDate(start,'month') + ' ' + FormatDate(start,'year');
+			date = date + FormatDate(start,'month') + ' ' + FormatDate(start,'year')
 		}
 
-		return date;
+		return {date, active}
 	}
 </script>
 
@@ -68,67 +70,78 @@
 	</div>
 </div>
 
+<!-- Curriculum vitae -->
 <div class="container pb-12">
-	<div>
-		<h2 class="text-4xl font-bold text-je-magical-fata_morgana font-poppins">{headings.main}</h2>
-		{#each headings.sections as section}
-			<h3 class="text-3xl font-bold text-je-magical-fata_morgana font-poppins">{section.title}</h3>
-			{#if section.key === 'skills'}
-				{#each items[section.key] as item}
-					<div class="pt-3">
-						<h4 class="text-2xl font-bold text-je-magical-fata_morgana font-poppins">{item.category}</h4>
-						{#if item.subcategories}
-							{#each item.subcategories as subcategory}
-								<div>
-									<h5 class="text-2xl font-bold font-poppins">{subcategory.title}</h5>
-									<div class="flex flex-wrap flex-row gap-1 text-sm font-montserrat">
-										{#each subcategory.skills as skill}
-											<Tags text={skill} />
-										{/each}
-									</div>
-								</div>
-							{/each}
-						{:else}
-							<div class="flex flex-wrap flex-row gap-1 text-sm font-montserrat">
-								{#each item.skills as skill}
-									<Tags text={skill} />
-								{/each}
-							</div>
-						{/if}
-					</div>
-				{/each}
-			{:else}
-				{#each items[section.key] as item}
-					<div class="my-6 flex items-start">
-						<div class="flex justify-center items-center rounded-xl min-w-16 min-h-16 {item.bgcolor} transition-transform duration-500 hover:scale-110">
-							<a href={item.link} target="_blank">
-								<Image src="/about_logos/{item.image}" alt="Logo von {item.company}" classNames="size-12" />
-							</a>
-						</div>
-						<div class="ml-10 flex flex-col justify-between">
-							<h4 class="text-2xl text-je-magical-fata_morgana font-poppins">{item.title}</h4>
-							<p class="text-xl font-montserrat">{item.company}</p>
-							<p class="text-md font-montserrat">{genDate(item?.start, item?.end)}</p>
-							{#if item.description[0]}
-								<div class="font-montserrat">
-									<ul class="list-disc text-md">
-										{#each item.description as desc}
-											<li>{desc}</li>
-										{/each}
-									</ul>
-								</div>
-							{/if}
-							{#if item.skills[0]}
-								<div class="flex flex-row flex-wrap gap-1 text-sm font-montserrat">
-									{#each item.skills as skill}
+	<h2 class="text-4xl font-bold text-je-magical-fata_morgana font-poppins">{headings.main}</h2>
+	{#each headings.sections as section}
+		<h3 class="text-3xl font-bold text-je-magical-fata_morgana font-poppins">{section.title}</h3>
+		{#if section.key == 'skills'}
+		<!-- Skills -->
+			{#each items[section.key] as item}
+				<div class="pt-3">
+					<h4 class="text-2xl font-bold text-je-magical-fata_morgana font-poppins">{item.category}</h4>
+					{#if item.subcategories}
+						{#each item.subcategories as subcategory}
+							<div>
+								<h5 class="text-2xl font-bold font-poppins">{subcategory.title}</h5>
+								<div class="flex flex-wrap flex-row gap-1 text-sm font-montserrat">
+									{#each subcategory.skills as skill}
 										<Tags text={skill} />
 									{/each}
 								</div>
-							{/if}
+							</div>
+						{/each}
+					{:else}
+						<div class="flex flex-wrap flex-row gap-1 text-sm font-montserrat">
+							{#each item.skills as skill}
+								<Tags text={skill} />
+							{/each}
 						</div>
-					</div>
-				{/each}
-			{/if}
-		{/each}
-	</div>
+					{/if}
+				</div>
+			{/each}
+		{:else}
+		<!-- Experience -->
+			<ol class="relative border-s border-gray-700 mt-10 font-montserrat">
+			{#each items[section.key] as item}
+				<li class="mb-10 ms-7">
+					<!-- Image -->
+					<a href={item.link} target="_blank">
+						<span class="absolute flex items-center justify-center size-8 rounded-full -start-4 ring-8 ring-je-mystical-nachtblau-500 {item.bgcolor} transition-transform duration-500 hover:scale-150 overflow-hidden">
+							<Image src="/about_logos/{item.image}" alt="Logo von {item.company}" classNames="size-5" />
+						</span>
+					</a>
+					<!-- Title -->
+					<h3 class="flex items-center mb-1 text-xl font-semibold text-white font-poppins">{item.title}
+						{#if valDate(item?.start, item?.end).active}
+							<span class="text-sm font-medium me-2 px-2.5 py-0.5 rounded bg-blue-900 text-blue-300 ms-3 font-montserrat">Aktuell</span>
+						{/if}
+					</h3>
+					<!-- Place -->
+					<p class="text-base font-normal text-gray-400">{item.company}</p>
+					<!-- Time -->
+					<time class="block mb-2 text-sm font-normal leading-none text-gray-500">{valDate(item?.start, item?.end).date}</time>
+					<!-- Description -->
+					{#if item.description[0]}
+						<div class="pl-4 mb-4 text-base font-normal text-gray-400">
+							<ul class="list-disc text-md">
+								{#each item.description as desc}
+									<li>{desc}</li>
+								{/each}
+							</ul>
+						</div>
+					{/if}
+					<!-- Skills -->
+					{#if item.skills[0]}
+						<div class="flex flex-row flex-wrap gap-1 text-sm">
+							{#each item.skills as skill}
+								<Tags text={skill} />
+							{/each}
+						</div>
+					{/if}
+				</li>
+			{/each}
+			</ol>
+		{/if}
+	{/each}
 </div>
